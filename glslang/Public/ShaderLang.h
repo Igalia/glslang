d86cdb123bff4df7167984a5b34385ce041b59dc
Neil Roberts <nroberts@igalia.com>
2018-03-20 17:41:05 +0100

    Add an option to set the base uniform location
    This will be used to generate uniform locations with --aml

--- glslang/Public/ShaderLang.h
+++ glslang/Public/ShaderLang.h
@@ -418,6 +418,7 @@ public:
     void setAutoMapBindings(bool map);
     void setAutoMapLocations(bool map);
     void addUniformLocationOverride(const char* name, int loc);
+    void setUniformLocationBase(int base);
     void setInvertY(bool invert);
     void setHlslIoMapping(bool hlslIoMap);
     void setFlattenUniformArrays(bool flatten);
